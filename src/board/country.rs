7ccb3ed8d0606c54f0f36868eb0639a033c79867
FonsDijkstra <fons.dijkstra@gmail.com>
2017-04-14 16:33:29 +0200

    Added africa to the map

--- src/board/country.rs
+++ src/board/country.rs
@@ -1,7 +1,7 @@
 extern crate petgraph;
 
 use board::card::Strength;
-use board::country::petgraph::graph::{UnGraph, NodeIndex, NodeIndices};
+use self::petgraph::graph::{UnGraph, NodeIndex, NodeIndices};
 
 pub type CountryId = NodeIndex;
 type CountryIterator = NodeIndices;
@@ -26,16 +26,35 @@ pub struct Map {
 impl Map {
     pub fn new() -> Map {
         let mut graph = UnGraph::<_, ()>::new_undirected();
+
         let venezuela = graph.add_node(Country { name: "Venezuela", strength: Strength::Artillery }); 
         let brazil = graph.add_node(Country { name: "Brazil", strength: Strength::Artillery });
         let peru = graph.add_node(Country { name: "Peru", strength: Strength::Cavelry });
         let argentina = graph.add_node(Country { name: "Argentine", strength: Strength::Infantry });
+
+        let egypt = graph.add_node(Country { name: "Egypt", strength: Strength::Infantry });
+        let east_africa = graph.add_node(Country { name: "East Africa", strength: Strength::Artillery });
+        let north_africa = graph.add_node(Country { name: "North Africa", strength: Strength::Infantry});
+        let south_africa = graph.add_node(Country { name: "South Africa", strength: Strength::Artillery});
+        let central_africa = graph.add_node(Country { name: "Central Africa", strength: Strength::Cavelry });
+        let madagascar = graph.add_node(Country { name: "Madagascar", strength: Strength::Infantry });
 
         graph.add_edge(venezuela, peru, ());
         graph.add_edge(venezuela, brazil, ());
         graph.add_edge(peru, brazil, ());
         graph.add_edge(peru, argentina, ());
         graph.add_edge(brazil, argentina, ());
+
+        graph.add_edge(north_africa, brazil, ());
+        graph.add_edge(north_africa, egypt, ());
+        graph.add_edge(north_africa, east_africa, ());
+        graph.add_edge(north_africa, central_africa, ());
+        graph.add_edge(egypt, east_africa, ());
+        graph.add_edge(east_africa, central_africa, ());
+        graph.add_edge(east_africa, madagascar, ());
+        graph.add_edge(east_africa, south_africa, ());
+        graph.add_edge(central_africa, south_africa, ());
+        graph.add_edge(madagascar, south_africa, ());
 
         Map { graph: graph }
     }
